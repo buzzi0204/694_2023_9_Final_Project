9968cd693cc46ecbc561db62f4310561672fdc01
Urjit Patil <patilurjit16@gmail.com>
2023-04-03 16:08:57 -0400

    extract required data from json objects

---
+++ insert_tweet_retweet.py
@@ -0,0 +1,44 @@
+import json
+
+with open("json_files/tweet_retweet_quote_trial.json", "r") as f:
+    data = json.loads(f.read())
+
+key_list = ['id', 'id_str', 'name', 'screen_name', 'location', 'url', 'description', 'verified', 
+            'created_at', 'followers_count', 'friends_count', 'favourites_count', 'statuses_count',
+            'lang']
+
+val_dict = {}
+rt_val_dict = {}
+qt_val_dict = {}
+
+for i in range(len(data)):
+    val_list = []
+
+    for key in key_list:
+        val_list.append(data[i]['user'][key])
+    
+    val_dict[i] = tuple(val_list)
+
+    if 'retweeted_status' in data[i]:
+        rt_val_list = []
+        for key in key_list:
+            rt_val_list.append(data[i]['retweeted_status']['user'][key])
+    else:
+        continue
+    
+    rt_val_dict[i] = tuple(rt_val_list)
+
+    if 'quoted_status' in data[i]:
+        qt_val_list = []
+        for key in key_list:
+            qt_val_list.append(data[i]['quoted_status']['user'][key])
+    else:
+        continue
+
+    qt_val_dict[i] = tuple(qt_val_list)
+
+print(len(val_dict))
+print(len(rt_val_dict))
+print(len(qt_val_dict))
+
+# print(qt_val_dict)
