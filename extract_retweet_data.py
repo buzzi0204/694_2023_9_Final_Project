dcd27ec9a07be1d57aada6984806291a968cd0ca
Urjit Patil <patilurjit16@gmail.com>
2023-04-03 23:54:37 -0400

    extract data from retweeted tweets

--- extract_retweet_data.py
+++ extract_retweet_data.py
@@ -7,19 +7,23 @@ with open("make_json_trial.json", "r") as f:
 val_dict = {}
 
 for i in range(len(data)):
-    val_list = []
+    if 'retweeted_status' in data[i]:
+        val_list = []
 
-    val_list.append(data[i]['id'])
-    val_list.append(data[i]['retweeted_status']['id'])
-    val_list.append(data[i]['user']['id'])
-    val_list.append(pd.to_datetime(data[i]['created_at']))
+        val_list.append(data[i]['id'])
+        val_list.append(data[i]['retweeted_status']['id'])
+        val_list.append(data[i]['user']['id'])
+        val_list.append(pd.to_datetime(data[i]['created_at']))
 
-    val_dict[i] = tuple(val_list)
+        val_dict[i] = tuple(val_list)
 
-    # try:
-    #     cur.execute(query_insert, val_list)
-    # except Exception as e:
-    #     print(e)
-    #     pass
+        # try:
+        #     cur.execute(query_insert, val_list)
+        # except Exception as e:
+        #     print(e)
+        #     pass
+
+    else:
+        continue
 
 print(len(val_dict))
