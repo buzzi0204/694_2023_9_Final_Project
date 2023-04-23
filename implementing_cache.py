0ede117a84d280a356064f28fba3b3be80aba14f
Urjit Patil <patilurjit16@gmail.com>
2023-04-23 17:41:36 -0400

    updated removal process

--- implementing_cache.py
+++ implementing_cache.py
@@ -1,16 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 15 19:43:13 2023
-
-@author: athar
-"""
-
 import pickle
+import time
 
 class Cache:
     def __init__(self, checkpoint_file=None, checkpoint_interval=None):
         self.max_size = 10
         self.cache = {}
+        self.key_times = []
         self.checkpoint_file = checkpoint_file
         self.checkpoint_interval = checkpoint_interval
         if checkpoint_file:
@@ -24,10 +19,13 @@ class Cache:
 
     def set(self, key, value):
         if len(self.cache) >= self.max_size:
-            oldest_key = next(iter(self.cache))
+            # Remove the oldest key
+            oldest_key = min(self.key_times, key=lambda x: x[1])[0]
+            self.key_times = [x for x in self.key_times if x[0] != oldest_key]
             self.cache.pop(oldest_key)
 
         self.cache[key] = value
+        self.key_times.append((key, time.time()))
 
         if self.checkpoint_file and self.checkpoint_interval and len(self.cache) % self.checkpoint_interval == 0:
             self.save_checkpoint()
@@ -40,5 +38,6 @@ class Cache:
         try:
             with open(self.checkpoint_file, 'rb') as file:
                 self.cache = pickle.load(file)
+                self.key_times = [(k, time.time()) for k in self.cache.keys()]
         except FileNotFoundError:
             pass
