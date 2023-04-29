49d4c28b6959affeb909bcc43148a459e4fbca9f
Urjit Patil <patilurjit16@gmail.com>
2023-04-28 20:03:54 -0400

    error handling

--- app.py
+++ app.py
@@ -26,27 +26,45 @@ def home():
 
 @app.route("/search", methods=["GET", "POST"])
 def helloworld():
-    if request.method == "POST":
-        option = request.form["options"]
-        query = request.form["query"]
-    if option == "keyword":
-        data = search.get_keyword(query)
-    elif option == "hashtag":
-        data = search.get_hashtag(query)
-    else:
-        data = search.get_username(query)
-    return render_template("/html/query.html", name=query, value=data.to_html())
-
+    try:
+        if request.method == "POST":
+            option = request.form["options"]
+            query = request.form["query"]
+        if option == "keyword":
+            start_time = time()
+            data = search.get_keyword(query)
+            end_time = time()
+            print(f"The time taken for the execution of the query: {end_time - start_time} second(s)")
+        elif option == "hashtag":
+            start_time = time()
+            data = search.get_hashtag(query)
+            end_time = time()
+            print(f"The time taken for the execution of the query: {end_time - start_time} second(s)")
+        else:
+            start_time = time()
+            data = search.get_username(query)
+            end_time = time()
+            print(f"The time taken for the execution of the query: {end_time - start_time} second(s)")  
+                
+        return render_template("/html/query.html", name=query, value=data.to_html())
+    except Exception as e:
+        return render_template("/html/missing.html")
 
 @app.route("/top_10_tweets")
 def top_10_tweets():
+    start_time = time()
     data = search.get_top_10_tweets()
+    end_time = time()
+    print(f"The time taken for the execution of the query: {end_time - start_time} second(s)")
     return render_template("/html/top_10.html", value=data.to_html())
 
 
 @app.route("/top_10_users")
 def top_10_users():
+    start_time = time()
     data = search.get_top_10_users()
+    end_time = time()
+    print(f"The time taken for the execution of the query: {end_time - start_time} second(s)")
     return render_template("/html/top_10.html", value=data.to_html())
 
 
